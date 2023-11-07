9723a17d084cf9e975ac14b1786224a522b52fbe
Raul Madero <r.madero.ramirez@gmail.com>
2023-11-07 13:11:03 -0600

    Se agregó el index

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   const excuse = document.querySelector("#excuse");
